f3527230b5a2acbc14de11642da33b48cb217937
Hafiz <hasnathafiz229>
2021-09-12 16:04:31 +0600

    final commit

---
+++ summary.js
@@ -0,0 +1,11 @@
+alert("");
+confirm("are you ready?");
+prompt("name");
+
+// location.reload()
+// location.href = ""
+// location.hash =
+// http://127.0.0.1:5500/shopping.html
+
+// history.back()
+// history.go()
